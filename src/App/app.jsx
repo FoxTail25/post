3562fcf7239a41d09982bfb938c3f0fbbdd56ca4
Grigoriy <foxtail25@gmail.com>
2022-12-27 07:15:15 +0300

    Насыпал чуток снега

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -8,6 +8,8 @@ import { Footer } from "../Footer/footer"
 import { Header } from "../Header/header"
 import { PostList } from "../PostList/post-list"
 import { postData } from "./posts"
+// import s from './app.module.css'
+import { } from './app.css'
 
 
 
@@ -48,40 +50,96 @@ const App = () => {
 
     return (
         <>
-            <CssBaseline/>
+            <CssBaseline />
 
-                <Header />
-                <Stack>
+            <Header />
 
-                    <Container sx={{
-                        display: "flex",
-                        flexWrap: 'wrap',
-                        justifyContent: "center",
-                        gap: '10px',
-                        marginTop: '10px',
-                        // height: '100vh',
+            <div className='snow s1'></div>
+            <div className='snow s2'></div>
+            <div className='snow s3'></div>
+            <div className='snow s4'></div>
+            <div className='snow s5'></div>
+            <div className='snow s6'></div>
+            <div className='snow s7'></div>
+            <div className='snow s8'></div>
+            <div className='snow s9'></div>
+            <div className='snow s10'></div>
+            <div className='snow s11'></div>
+            <div className='snow s12'></div>
+            <div className='snow s13'></div>
+            <div className='snow s14'></div>
+            <div className='snow s15'></div>
+            <div className='snow s16'></div>
+            <div className='snow s17'></div>
+            <div className='snow s18'></div>
+            <div className='snow s19'></div>
+            <div className='snow s20'></div>
+            <div className='snow s21'></div>
+            <div className='snow s22'></div>
+            <div className='snow s23'></div>
+            <div className='snow s24'></div>
+            <div className='snow s25'></div>
+            <div className='snow s26'></div>
+            <div className='snow s27'></div>
+            <div className='snow s28'></div>
+            <div className='snow s29'></div>
+            <div className='snow s31'></div>
+            <div className='snow s34'></div>
+            {/* <div className='snow s30'></div>
+            <div className='snow s32'></div> */}
+            {/* <div className='snow s33'></div>
+            <div className='snow s35'></div> */}
+            {/* <div className='snow s36'></div>
+        <div className='snow s39'></div> */}
+            <div className='snow s37'></div>
+            <div className='snow s38'></div>
+            <div className='snow s40'></div>
+            <div className='snow s41'></div>
+            <div className='snow s42'></div>
+            <div className='snow s43'></div>
+            <div className='snow s44'></div>
+            <div className='snow s45'></div>
+            <div className='snow s46'></div>
+            <div className='snow s47'></div>
+            <div className='snow s48'></div>
+            <div className='snow s49'></div>
+            <div className='snow s50'></div>
 
-                    }} maxWidth='xl'>
-                        <Banner />
-                        <PostList postData={dataState} />
-                    </Container>
 
-                    <Container sx={{
-                        display: "flex",
-                        flexWrap: 'wrap',
-                        justifyContent: "center",
-                        padding: '15px',
-                        mb: '10%',
-                    }}>
+            <Stack>
 
-                        <Pagination count={pagePostCount} color="primary" onChange={(event, num) => postPage(num)} />
+                <Container sx={{
+                    display: "flex",
+                    flexWrap: 'wrap',
+                    justifyContent: "center",
+                    gap: '10px',
+                    marginTop: '10px',
+                    // height: '100vh',
+
+                }} maxWidth='xl'>
+                    <Banner />
+                    <PostList postData={dataState} />
+                </Container>
+
+                <Container sx={{
+                    display: "flex",
+                    flexWrap: 'wrap',
+                    justifyContent: "center",
+                    padding: '15px',
+                    mb: '10%',
+                }}>
+
+                    <Pagination count={pagePostCount} color="primary" onChange={(event, num) => postPage(num)} sx={{
+                        background: 'white',
+                        borderRadius: '10px'
+                    }} />
 
 
-                    </Container >
+                </Container >
 
-                </Stack>
+            </Stack>
 
-                <Footer />
+            <Footer />
         </>
     )
 }
