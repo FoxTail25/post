76933d1a47f87dbdab53ade65286868f654159c5
Grigoriy <foxtail25@gmail.com>
2022-12-28 04:03:11 +0300

    убрал импорт пагИтем

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -1,4 +1,4 @@
-import { Container, CssBaseline, Pagination, PaginationItem } from "@mui/material"
+import { Container, CssBaseline, Pagination,  } from "@mui/material"
 import { Stack } from "@mui/system"
 import React from "react"
 import { useEffect } from "react"
