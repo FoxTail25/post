dd7a4386183f93ecdbd17eb55401fc7d5b6270fb
Grigoriy <foxtail25@gmail.com>
2023-01-13 01:15:39 +0300

    комменты готовы

--- src/Pages/post-pages/postPages.jsx
+++ src/Pages/post-pages/postPages.jsx
@@ -1,14 +1,14 @@
 import { Avatar, Badge, Button, Card, CardContent, CardHeader, CardMedia, IconButton, Typography } from "@mui/material"
 import { useContext, useEffect, useState } from "react"
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 import { AllContextData } from "../../components/context/context"
+import { Link, useParams } from "react-router-dom";
+import { PostTag } from "../../components/PostTag/post-tag";
+import PostComment from "../../components/PostCommentsList/PostCommentsList";
 import api from "../../utils/api"
 import dayjs from "dayjs";
-import { Link, useParams } from "react-router-dom";
 import s from './postPage.module.css'
-import { PostTag } from "../../components/PostTag/post-tag";
-import PostComment from "../../components/PostComments/PostComments";
-// import parse from 'html-react-parser';
 
 
 export const PostPage = () => {
@@ -22,7 +22,9 @@ export const PostPage = () => {
 
     useEffect(() => { api.getPostById(postIdFromUrl.postId).then((data) => { setSinglePost(data) }) }, [changeStateLikedPost, postIdFromUrl.postId])
 
-    const { _id, author, created_at, image, title, text, likes, comments, tags, } = singlePost
+    const { _id, author, created_at, image, title, text, likes, comments, tags } = singlePost
+
+
 
     let color
 
@@ -59,12 +61,11 @@ export const PostPage = () => {
 
                                 subheader={dayjs(created_at).format('HH:MM:s DD/MM/YYYY')}
                             />
-                    
-                                {tags
-                                    ? <PostTag tags={tags} />
-                                    : 0
-                                }
-                            
+
+
+                            <div style={{display: 'flex'}}>
+                                {tags.map(e => <div style={{padding:"0px 4px"}}>#{e}</div>)}
+                            </div>
                             <CardMedia
                                 component="img"
                                 height="auto"
@@ -75,28 +76,34 @@ export const PostPage = () => {
 
 
                             <CardContent sx={{ flex: 1 }}>
+
                                 <Typography variant="h5" color="text.secondary">
                                     {title}
                                 </Typography>
-                                {/* <Typography variant="body2" color="text.secondary"> */}
-                                   {/* {parse(toString(text))} */}
-                                   <p dangerouslySetInnerHTML={{__html: text}} />
-                                    {/* {parse(text, {
-        replace: ({ attribs }) => attribs && attribs.id === 'remove' && <></>
-        })} */}
-                                {/* </Typography> */}
+
+                                <p dangerouslySetInnerHTML={{ __html: text }} />
+
                             </CardContent>
 
                             <div className={s.cart__bottom}>
+
                                 <IconButton aria-label="add to favorites" color={color} onClick={function (e) { e.stopPropagation(); changeStateLikedPost(likes, _id) }}>
                                     <Badge badgeContent={likes?.length} color="primary">
                                         <FavoriteIcon />
                                     </Badge>
                                 </IconButton>
+
+                                <IconButton aria-label="edit post" >
+                                    <DriveFileRenameOutlineIcon />
+                                </IconButton>
+
                                 <Button variant="outlined" onClick={function (e) { e.stopPropagation(); deletePost(author, _id) }}>Удалить пост</Button>
+
                             </div>
 
-                            <PostComment comments={comments}/>
+                            {comments?.length
+                                ? <PostComment comments={comments} id={_id} />
+                                : null}
 
                         </Card>}
 
